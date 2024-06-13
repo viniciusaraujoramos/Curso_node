deabbc54c6c0319b555b67fd6eddc5525bde9641
viniciusaraujoramos <vinicius.ara.ramos@gmail.com>
2024-06-13 09:01:03 -0300

    post

--- 05_Handlebars/app.js
+++ 05_Handlebars/app.js
@@ -1,14 +1,29 @@
 const express = require('express')
 const app = express()
 const handlebars = require('express-handlebars')
+const Sequelize = require('sequelize')
 
 //configuração
     //Template Engine
-    app.engine('handlebars', handlebars({
-        
-    }))
+    app.engine('handlebars', handlebars.engine({defaultLayout:'main'}))
+    app.set('view engine', 'handlebars')
 
-app.listen(8081, function(){
-    console.log('Servidor rodando em http://localhost:8081')
+//Conexão com o  Banco de Dados
+const sequelize = new Sequelize('test','root','',{
+    host:'localhost',
+    dialect:'mysql'
+})
+
+//Rotas
+app.get('/cadastro',function(req,res){
+    // res.send('ROTA DE CADASTRO DE POSTS')
+    res.render('formulario')
+})
+
+app.post('/add',function(req,res){
+    res.send('Formulario recebido')
+})
+
+app.listen(8081,function(){
+    console.log('Servidor rodando na url http://localhost:8081')
 }) //criou servidor e está escutando a porta
-
