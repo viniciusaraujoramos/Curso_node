062ac2f1fb42f112f3333ad1d525c40da205b123
viniciusaraujoramos <vinicius.ara.ramos@gmail.com>
2024-06-13 08:02:25 -0300

    instal express handlebars

--- 05_Handlebars/app.js
+++ 05_Handlebars/app.js
@@ -1,5 +1,12 @@
 const express = require('express')
 const app = express()
+const handlebars = require('express-handlebars')
+
+//configuração
+    //Template Engine
+    app.engine('handlebars', handlebars({
+        
+    }))
 
 app.listen(8081, function(){
     console.log('Servidor rodando em http://localhost:8081')
